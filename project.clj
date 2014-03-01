45c4417e41e10a14669006414668dfbb7830cca5
gered <gered@blarg.ca>
2014-03-01 09:08:20 -0500

    initial commit, complete with "hello world" ... woohoo!

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject clj-jtwig "0.1.0-SNAPSHOT"
+  :description "Clojure wrapper for JTwig"
+  :url "https://github.com/gered/clj-jtwig"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                              :snapshots false}]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.lyncode/jtwig-core "2.1.0"]])
