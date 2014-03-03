117e7cb0798f1e648fcb0663428f8c39f3c2b489
gered <gered@blarg.ca>
2014-03-03 13:51:10 -0500

    don't actually need the sonatype repository listed for this now

--- project.clj
+++ project.clj
@@ -3,7 +3,5 @@
   :url "https://github.com/gered/clj-jtwig/tree/java6"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
-                              :snapshots false}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.lyncode/jtwig-core-java6 "2.1.1"]])
