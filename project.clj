17caf40f2a5147d8e269b5c8376bad018d842752
gered <gered@blarg.ca>
2014-03-04 16:55:36 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jtwig "0.2"
+(defproject clj-jtwig "0.2.1"
   :description "Clojure wrapper for JTwig"
   :url "https://github.com/gered/clj-jtwig"
   :license {:name "Apache License, Version 2.0"
