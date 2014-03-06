b120bc32701df10a2b7314e2b58bbc2ea1fbe1f8
gered <gered@blarg.ca>
2014-03-05 19:18:38 -0500

    not exactly sure how i messed up that merge...

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
-(defproject clj-jtwig "0.2.2"
-  :description "Clojure wrapper for JTwig"
-  :url "https://github.com/gered/clj-jtwig"
+(defproject clj-jtwig-java6 "0.2.2"
+  :description "Clojure wrapper for JTwig (Java 6 dependencies)"
+  :url "https://github.com/gered/clj-jtwig/tree/java6"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
-                              :snapshots false}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.lyncode/jtwig-core "2.1.1"]])
+                 [com.lyncode/jtwig-core-java6 "2.1.1"]])
