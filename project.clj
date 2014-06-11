8c4aeddf692f4330bdca58c7f8a3bf27baa9434d
gered <gered@blarg.ca>
2014-06-11 09:16:01 -0400

    update jtwig version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-jtwig "0.4"
+(defproject clj-jtwig "0.4.1"
   :description "Clojure wrapper for JTwig"
   :url "https://github.com/gered/clj-jtwig"
   :license {:name "Apache License, Version 2.0"
@@ -6,5 +6,5 @@
   :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                               :snapshots false}]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.lyncode/jtwig-core "2.1.4"]
+                 [com.lyncode/jtwig-core "2.1.7"]
                  [org.apache.commons/commons-lang3 "3.1"]])
