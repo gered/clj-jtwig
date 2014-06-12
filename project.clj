6406a31c988c0de2f4b1e9bac59d01c12fe76f32
gered <gered@blarg.ca>
2014-06-12 16:23:53 -0400

    clojure 1.6.0

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                               :snapshots false}]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.lyncode/jtwig-core "2.1.7"]
                  [org.apache.commons/commons-lang3 "3.1"]])
