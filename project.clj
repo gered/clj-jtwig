0891fa5675dd4bbb0a403edce6a82fe86d378482
gered <gered@blarg.ca>
2014-06-15 13:33:45 -0400

    reorg src dir to allow for some java source files

--- project.clj
+++ project.clj
@@ -6,5 +6,7 @@
   :repositories [["sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                               :snapshots false}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.apache.commons/commons-lang3 "3.1"]])
                  [com.lyncode/jtwig-core "3.0.0-SNAPSHOT"]
+                 [org.apache.commons/commons-lang3 "3.1"]]
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"])
